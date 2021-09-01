e9a50f2fd5c98cef08a60d0f8b96f5fb291c25a4
Leandro Koshoji Araujo <leandrokoshoji@gmail.com>
2021-08-31 21:30:14 -0300

    set global css

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,32 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+:root {
+  --clr-background: #f5f5f5;
+  --clr-primary: #2d3f51;
+  --clr-accent: #ff6854;
+  --clr-txt-primary: #2d3f51;
+  --clr-txt-secundary: #f5f5f5;
+}
+*{
+  padding: 0;
+  margin: 0;
+  font-family: 'Roboto', sans-serif;
+}
+body {
+  background-color: var(--clr-background);
+  color: var(--clr-txt-primary);
+  min-height: 100vw;
+  width: 100%;
+}
 
+a{
+  color: inherit;
+}
+
+.container {
+  width: Min(90% 70rem);
+  margin: 0 auto;
+}
 </style>
