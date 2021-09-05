b55dd00ee995c8dcb8628e9efc69580043b3632c
Leandro Koshoji Araujo <leandrokoshoji@gmail.com>
2021-09-05 17:37:08 -0300

    add login test

--- tests/unit/Login.spec.js
+++ tests/unit/Login.spec.js
@@ -3,9 +3,18 @@ import Login from '@/views/Login'
 import { mount } from '@vue/test-utils'
 
 describe('Login.vue', () =>  {
-    it('teste', () => {
+    it('userData receive email and password input value ', () => {
         const wrapper = mount(Login)
 
-        wrapper.exists()
+        const email = wrapper.find('#email')
+        const password = wrapper.find('#password')
+        
+        email.setValue('teste@teste.com')
+        password.setValue('123456')
+
+        expect(wrapper.vm.userData).toStrictEqual({
+            email: 'teste@teste.com', 
+            password: '123456'
+        })
     })
-}) 
+})
